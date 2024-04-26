a02315a26214e577fdacadb30960ad8022d6f513
Andrew Suprun <andrew.suprun@gmail.com>
2024-04-26 15:55:53 -0400

    playing around 0005

--- src/main.zig
+++ src/main.zig
@@ -25,20 +25,42 @@ const Node = struct {};
 
 const BoardSize = 19;
 
-const place_first = 0x01;
-const place_second = 0x10;
-
 const Board = struct {
-    places: [BoardSize][BoardSize]u8 = [1][BoardSize]u8{[1]u8{0} ** BoardSize} ** BoardSize,
+    places: [BoardSize][BoardSize]Player = [1][BoardSize]Player{[1]Player{.none} ** BoardSize} ** BoardSize,
 
     fn init() Board {
         var board = Board{};
-        board.places[BoardSize / 2][BoardSize / 2] = place_second;
+        board.places[BoardSize / 2][BoardSize / 2] = .second;
         return board;
+    }
+
+    fn get(self: Board, x: i8, y: i8) Player {
+        return self.places[@intCast(x)][@intCast(y)];
+    }
+
+    fn print(self: Board) void {
+        for (0..BoardSize) |j| {
+            for (0..BoardSize) |i| {
+                switch (self.places[i][j]) {
+                    .none => std.debug.print(". ", .{}),
+                    .first => std.debug.print("O ", .{}),
+                    .second => std.debug.print("X ", .{}),
+                }
+            }
+            std.debug.print("\n", .{});
+        }
     }
 };
 
-const Player = enum { first, second, none };
+const Player = enum(u8) { first = 0x01, second = 0x10, none = 0x00 };
+
+const Scores = struct {
+    places: [BoardSize][BoardSize]i32 = [1][BoardSize]i32{[1]i32{0} ** BoardSize} ** BoardSize,
+
+    fn get(self: Scores, x: i8, y: i8) i32 {
+        return self.places[@intCast(x)][@intCast(y)];
+    }
+};
 
 // pub fn rollout(allocator: Allocator, moves: []const Move, move: Move) std.AutoHashMap(Move, Node) {
 pub fn expand(moves: []const Move, nodes: std.AutoHashMap(Move, Node)) !void {
@@ -47,7 +69,7 @@ pub fn expand(moves: []const Move, nodes: std.AutoHashMap(Move, Node)) !void {
     const allocator = arena.allocator();
 
     var board = Board.init();
-    var player: u8 = place_first;
+    var player: Player = .first;
     var places_to_consider = std.AutoHashMap(Coord, void).init(allocator);
     try add_places_to_consider(Coord{ .x = BoardSize / 2, .y = BoardSize / 2 }, &places_to_consider);
 
@@ -58,7 +80,7 @@ pub fn expand(moves: []const Move, nodes: std.AutoHashMap(Move, Node)) !void {
         try add_places_to_consider(m[0], &places_to_consider);
         try add_places_to_consider(m[1], &places_to_consider);
 
-        player = place_first + place_second - player;
+        player = if (player == .first) .second else .first;
     }
 
     for (moves) |m| {
@@ -139,6 +161,30 @@ fn add_places_to_consider(place: Coord, places: *std.AutoHashMap(Coord, void)) !
     }
 }
 
+fn score_place(board: Board, scores: *Scores, place: Coord) void {
+    for (config.row_idces[place.x][place.y]) |idx| {
+        score_row(board, scores, config.row_config[idx]);
+    }
+}
+
+fn score_row(board: Board, scores: *Scores, row_config: RowConfig) void {
+    print("{}:{} {}:{} {}: ", .{ row_config.x, row_config.y, row_config.dx, row_config.dy, row_config.count });
+    if (row_config.count == 0) return;
+
+    var sum = @intFromEnum(board.get(row_config.x, row_config.y)) +
+        @intFromEnum(board.get(row_config.x, row_config.y)) +
+        @intFromEnum(board.get(row_config.x + 2 * row_config.dx, row_config.y + 2 * row_config.dy)) +
+        @intFromEnum(board.get(row_config.x + 3 * row_config.dx, row_config.y + 3 * row_config.dy)) +
+        @intFromEnum(board.get(row_config.x + 4 * row_config.dx, row_config.y + 4 * row_config.dy)) +
+        @intFromEnum(board.get(row_config.x + 5 * row_config.dx, row_config.y + 5 * row_config.dy));
+
+    print("sum = {}\n", .{sum});
+    sum += 1;
+    _ = scores;
+
+    // TODO: Finish
+}
+
 test Board {
     var moves = std.ArrayList(Move).init(std.testing.allocator);
     // try moves.append([2]Coord{ .{ .x = 0, .y = 0 }, .{ .x = 0, .y = 0 } });
@@ -148,10 +194,29 @@ test Board {
 
     const nodes = std.AutoHashMap(Move, Node).init(std.testing.allocator);
     _ = try expand(moves.items, nodes);
+
+    for (config.row_config, 0..) |c, i| {
+        print("{}: start: {}:{} delta: {}:{} count: {}\n", .{ i, c.x, c.y, c.dx, c.dy, c.count });
+    }
+
+    print("\n\n", .{});
+
+    for (0..BoardSize) |i| {
+        for (0..BoardSize) |j| {
+            const r = config.row_idces[i][j];
+            print("{}:{} - {} {} {} {}\n", .{ i, j, r[0], r[1], r[2], r[3] });
+        }
+    }
+
+    const board = Board.init();
+    var scores = Scores{};
+
+    score_place(board, &scores, Coord{ .x = 7, .y = 7 });
+    board.print();
 }
 
 const config = GameConfig.init();
-const row_count: i32 = 6 * BoardSize - 22;
+const row_count: i32 = 6 * BoardSize - 21;
 const RowConfig = struct { x: i8, y: i8, dx: i8, dy: i8, count: i8 };
 const RowIndices = [BoardSize][BoardSize][4]u32;
 const GameConfig = struct {
@@ -163,7 +228,7 @@ const GameConfig = struct {
         var row_config = [_]RowConfig{.{ .x = 0, .y = 0, .dx = 0, .dy = 0, .count = 0 }} ** row_count;
         var row_indices = [_][BoardSize][4]u32{[_][4]u32{[_]u32{ 0, 0, 0, 0 }} ** BoardSize} ** BoardSize;
 
-        var row_idx: u32 = 0;
+        var row_idx: u32 = 1;
         for (0..BoardSize) |i| {
             row_config[row_idx] = RowConfig{ .x = 0, .y = @intCast(i), .dx = 1, .dy = 0, .count = BoardSize - 5 };
 
