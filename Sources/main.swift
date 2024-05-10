6de548ea60b7fb8358703fb8b64e49b79e5af939
Andrew Suprun <andrew.suprun@gmail.com>
2024-05-09 20:19:08 -0400

    Small progress.

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,30 +4,34 @@ protocol Default {
   init()
 }
 
-protocol ScoreProtocol: Default {
-  var double: Double { get }
+enum Player { case first, second }
+
+protocol MoveProtocol {
+  var player: Player { get }
+  var score: Float { get }
+  
+  init(player: Player, score: Float)
 }
 
 protocol GameProtocol: Default {
-  associatedtype Move: Default
-  associatedtype Score: ScoreProtocol
+  associatedtype Move: MoveProtocol
 
-  static var exploreFactor: Double { get }
-  func make(move: Move)
-  func extend() -> [(move: Move, score: Score)]
+  static var exploreFactor: Float { get }
+  mutating func make(move: Move)
+  func extend() -> [Move]
 }
-
-enum Player { case first, second }
 
 struct SearchTree<Game: GameProtocol> {
   class Node {
     var children = [Node]()
-    var score: Game.Score = Game.Score()
-    var move: Game.Move = Game.Move()
-    init() {}
-    init(move: Game.Move, score: Game.Score) {
+    var move: Game.Move
+    
+    init() {
+      move = Game.Move(player: .second, score: 0)
+    }
+
+    init(move: Game.Move) {
       self.move = move
-      self.score = score
     }
   }
 
@@ -36,7 +40,8 @@ struct SearchTree<Game: GameProtocol> {
   func extend() {
     var game = Game()
     let leaf = selectLeaf(game: &game)
-    leaf.children = game.extend().map { Node(move: $0.move, score: $0.score) }
+    let moves = game.extend()
+    leaf.children = moves.map { Node(move: $0) }
   }
 
   func selectLeaf(game: inout Game) -> Node {
@@ -48,23 +53,24 @@ struct SearchTree<Game: GameProtocol> {
         node = child
         game.make(move: node.move)
         player = if player == .first { .second } else { .first }
+      } else {
+        return node
       }
     }
-    return node
   }
 
   func selectChild(parent: Node, player: Player) -> Node? {
     guard !parent.children.isEmpty else { return nil }
-    let dParentChildren = Double(parent.children.count)
+    let dParentChildren = Float(parent.children.count)
 
     var selectedChild = parent.children[0]
-    let dChildren = Double.init(selectedChild.children.count)
-    let dScore = selectedChild.score.double
+    let dChildren = Float.init(selectedChild.children.count)
+    let dScore = selectedChild.move.score
     var selectedScore = dScore + Game.exploreFactor * sqrt(dParentChildren / dChildren)
 
     for child in parent.children.dropFirst() {
-      let dChildren = Double.init(selectedChild.children.count)
-      let dScore = selectedChild.score.double
+      let dChildren = Float.init(selectedChild.children.count)
+      let dScore = selectedChild.move.score
       let score = dScore + Game.exploreFactor * sqrt(dParentChildren / dChildren)
 
       if (player == .first && selectedScore < score) || (player == .second && selectedScore > score)
@@ -75,43 +81,65 @@ struct SearchTree<Game: GameProtocol> {
     }
     return selectedChild
   }
+
+  func debug() {
+    debug(node: root, level: 0)
+  }
+
+  func debug(node: Node, level: Int) {
+    for _ in 0..<level {
+      print("| ", terminator: "")
+    }
+    print(node.move)
+    for child in node.children {
+      debug(node: child, level: level + 1)
+    }
+  }
 }
 
 /// Test Game
 
-struct TestMove: Default {
-  init() {}
+struct TestGame: GameProtocol {
+  typealias Move = TestMove
+
+  static let exploreFactor: Float = 2.0
+  var currentPlayer: Player = .first
+
+  mutating func make(move: Move) {
+    currentPlayer = if currentPlayer == .first { .second } else { .first }
+  }
+
+  func extend() -> [Move] {
+    var moves = [TestMove]()
+    for _ in 1...5 {
+      let score = if currentPlayer == .first { Float(Int.random(in: 10...20)) } else { -Float(Int.random(in: 10...20)) }
+      moves.append(TestMove(player: currentPlayer, score: score))
+    }
+    return moves
+  }
 }
 
-extension Int32: ScoreProtocol {
-  init() {
-    self = 0
-  }
-  var double: Double { Double(self) }
+struct TestMove: MoveProtocol, CustomStringConvertible {
+  static var lastId: Int = 0
+  let id: Int
+  let player: Player
+  let score: Float
 
+  init(player: Player, score: Float) {
+    Self.lastId += 1
+    id = Self.lastId
+    self.player = player
+    self.score = score
+  }
+
+  var description: String {
+    return "#\(id): \(player) [\(score)]"
+  }
 }
 
 var tree = SearchTree<TestGame>()
-struct TestGame: GameProtocol {
-  typealias Move = TestMove
-  typealias Score = Int32
+tree.extend()
+tree.extend()
+tree.extend()
+tree.debug()
 
-  static let exploreFactor = 2.0
-
-  func make(move: Move) {}
-  func extend() -> [(move: Move, score: Score)] {
-    return []
-  }
-}
-
-enum Foo {
-  case c(Character)
-  case b(UInt8)
-  case d(String)
-  case e([Int])
-  case z(Character)
-  case x
-  case y(Int)
-}
-
-print(MemoryLayout<Foo>.size, MemoryLayout<Foo>.alignment)
